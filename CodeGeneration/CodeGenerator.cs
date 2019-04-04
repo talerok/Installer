74786e22a97d22e7c86d8e0b51ea4d7cc0337e73
Artem Kurochkin <a.kurochkin@sigma-it.ru>
2019-04-04 18:02:53 +0500

    + CodeGeneration Project

---
+++ CodeGeneration/CodeGenerator.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+namespace CodeGeneration
+{
+    public class ScriptGlobal<T>
+    {
+        public T Global { get; }
+
+        public string GenerateFromFile<E>(string path, E global)
+        {
+            return CodeGeneration.CodeGenerator.GenerateFromFile(path, global).Result;
+        }
+
+        public ScriptGlobal(T global)
+        {
+            Global = global;
+        }
+    }
+
+    public static class CodeGenerator
+    {
+        private const string _commentPattern = @"/\*\W*[GENERATE]([\w\W]*?)\*/";
+        private const string _funcPattern = @"<- *\{([\w\W]*)\}";
+
+        private static async Task<string> _compileFuncs<T>(IEnumerable<string> funcs, T global)
+        {
+            var code = new StringBuilder();
+            code.AppendLine("using System;");
+            code.AppendLine("using System.Text;");
+            code.AppendLine("using System.Collections.Generic;");
+            code.AppendLine("using System.Linq;");
+
+            code.AppendLine("var funcs = new List<Func<string>>();");
+            foreach (var func in funcs)
+                code.AppendLine($@"funcs.Add(() => {{{func}}});");
+
+            code.AppendLine(@"String.Join(""\n"", funcs.Select(x => x()))");
+
+            return await CSharpScript.EvaluateAsync<string>(
+                code: code.ToString(),
+                globals: new ScriptGlobal<T>(global),
+                globalsType: typeof(ScriptGlobal<T>),
+                options: ScriptOptions.Default.AddReferences(
+                    typeof(System.Linq.Enumerable).Assembly
+                ));
+        }
+        
+        private static async Task<string> _formatCode<T>(string code, T global)
+        {
+            var res = new StringBuilder(code);
+            var offset = 0;
+
+            foreach(Match comment in Regex.Matches(code, _commentPattern))
+            {
+                var commentRes = new List<string>();
+                foreach (Match func in Regex.Matches(comment.Groups[1].Value, _funcPattern))
+                    commentRes.Add(func.Groups[1].Value);
+                res.Remove(comment.Index + offset, comment.Length);
+
+                var test = res.ToString();
+
+                string formatedCode = await _compileFuncs(commentRes, global);
+                res.Insert(comment.Index + offset, formatedCode);
+                offset += formatedCode.Length - comment.Length;
+            }
+            return res.ToString();
+        }
+
+        public static async Task<string> GenerateFromFile<T>(string filePath, T global)
+        {
+            return await _formatCode(File.ReadAllText(filePath), global);
+        }
+    }
+}
